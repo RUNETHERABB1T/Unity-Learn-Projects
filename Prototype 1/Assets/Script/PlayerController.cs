f5a059444df391c3796307ef410e7f58fd4fa416
RUNETHERABB1T <48692061+RUNETHERABB1T@users.noreply.github.com>
2023-06-20 10:55:02 +0200

    UPLOAD YOU FUCKING NERD
    WHY IS YOU BE BITCH???

--- Prototype 1/Assets/Script/PlayerController.cs
+++ Prototype 1/Assets/Script/PlayerController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        //Vehicle movement
+        transform.Translate(0, 0, 1);
     }
 }
